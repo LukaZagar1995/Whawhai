0c287e2efb21e83bcd90151a40f40975c466dca3
Luka Žagar <lukazagar95@gmail.com>
2019-11-12 07:21:15 +0100

    Refactor

--- src/constants/avatarConstants.js
+++ src/constants/avatarConstants.js
@@ -3,26 +3,26 @@ export const API_BASE_URL = "";
 export const AVATAR = [{
     name: "Marvin",
     attacks: ["Depression #1", "Depression #2", "Depression #3"],
-    image: require("../../assets/avatar_marvin.svg")
+    image: require("../../assets/images/avatar_marvin.svg")
 },
 {
     name: "Dragon",
     attacks: ["Spitting", "Dragon-Claw", "Pound"],
-    image: require("../../assets/avatar_dragon.svg")
+    image: require("../../assets/images/avatar_dragon.svg")
 },
 {
     name: "Tesla",
     attacks: ["Shaker-Machine", "AC voltage", "Radio Waves"],
-    image: require("../../assets/avatar_tesla.svg")
+    image: require("../../assets/images/avatar_tesla.svg")
 },
 {
     name: "Hackerman",
     attacks: ["Cyber", "System32 Error", "Norton Antivirus"],
-    image: require("../../assets/avatar_hackerman.svg")
+    image: require("../../assets/images/avatar_hackerman.svg")
 },
 {
     name: "Unicorn",
     attacks: ["Rainbowshit", "Candypuke", "Cornlove"],
-    image: require("../../assets/avatar_unicorn.svg")
+    image: require("../../assets/images/avatar_unicorn.svg")
 },
 ]
